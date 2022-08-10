bfb3928f190fee0b0108032c307f000892a933b1
clic <s172726@163.com>
2022-08-10 16:49:16 +0800

    pages

---
+++ Pages/src/router/index.js
@@ -0,0 +1,23 @@
+//引入路由对象
+import { createRouter, createWebHistory, createWebHashHistory, createMemoryHistory, RouteRecordRaw } from 'vue-router'
+
+//路由数组的类型 RouteRecordRaw
+// 定义一些路由
+// 每个路由都需要映射到一个组件。
+const routes: Array<RouteRecordRaw> = [{
+    path: '/',
+    component: () => import('../components/a.vue')
+},{
+    path: '/register',
+    component: () => import('../components/b.vue')
+}]
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+//导出router
+export default router
